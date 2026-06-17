890de2cc1b27b9c51adde14edce8a6b9be01bc68
web dev <154662817+D3XH3R@users.noreply.github.com>
2026-06-17 08:10:18 +0300

    made the reading function loop to itself so it can listen again!

--- practice/server.cpp
+++ practice/server.cpp
@@ -1,20 +1,3 @@
-/*
-* ASIO/server BLUEPRINT
-* 1. Setup:
-*	- io_context: The core I/O object that manages asynchronous operations.
-*	- Acceptor: Listens for
-* 2. Listing loop:
-*	- You need a function that constantly loops to accept new guests.
-*	- The Concept: Create a blank "socket room" (connection object), and tell the OS: "When someone knocks, put them in this room and call my handle_accept function."
-*	- The Critical Trick: Inside handle_accept, you must immediately call your listen function again so the guard goes back to the door for the next guest.
-* 3. The Object Lifetime Guard Step
-*	- Asynchronous code means your functions return immediately while the OS works in the background. If your connection object dies while the OS is writing to it, the program crashes.
-*	- The Concept: The connection object must hold onto itself using shared_from_this() until the OS says, "I'm done sending the data."
-* 4. The Trigger Step
-*	- Nothing actually happens until you turn the key.
-*	- The Concept: Call io_context.run(). This starts the background loop that listens for the OS to say "I'm done!" and executes your callbacks.
-*/
-
 #include <iostream>
 #include <thread>
 #include <boost/asio.hpp>
@@ -26,6 +9,8 @@
 using namespace std;
 using boost::asio::ip::tcp;
 
+
+int increment_instance = 0;
 class chat_room;
 class chat_session : public enable_shared_from_this<chat_session> {
 public:
@@ -38,7 +23,7 @@ public:
 		auto shared_msg = make_shared<string>(msg);
 		boost::asio::async_write(socket_,boost::asio::buffer(*shared_msg), [self = shared_from_this(), shared_msg](const boost::system::error_code& error, size_t bytes_transferred) {
 			if (!error) {
-				cout << "Sent: " << self->send_buffer_ << endl;
+				cout << "Sent: " << string(*shared_msg) << endl;
 			}
 			else {
 				cerr << "ServerSendError: " << error.message() << endl;
@@ -48,12 +33,18 @@ public:
 	tcp::socket& socket() {
 		return socket_;
 	};
-	chat_session(boost::asio::io_context& io_context, chat_room& room_ptr) : socket_(io_context), room(room_ptr) {};
+	int get_instance_number() {
+		return instance_number;
+	};
+	chat_session(boost::asio::io_context& io_context, chat_room& room_ptr) : socket_(io_context), room(room_ptr) {
+		instance_number = increment_instance;
+	};
 private:
 	tcp::socket socket_;
 	string send_buffer_;
 	array<char, 1024> recv_buffer_;
 	chat_room& room;
+	int instance_number = 0;
 };
 
 class chat_room {
@@ -64,9 +55,11 @@ public:
 	void leave(shared_ptr<chat_session> session) {
 		sessions_.erase(remove(sessions_.begin(), sessions_.end(), session), sessions_.end());
 	};
-	void broadcast(string msg) {
+	void broadcast(string msg, int& instance_num_param) {
 		for (auto& session : sessions_) {
-			if (session->socket().is_open()) {
+			if (session->get_instance_number() == instance_num_param)
+				continue;
+			if (session->socket().is_open() && session->get_instance_number() != instance_num_param) {
 				session->self_send(msg);
 			}
 			else {
@@ -83,10 +76,13 @@ private:
 };
 
 void chat_session::start() {
+	cout << "reading user message" << endl;
 	boost::asio::async_read(socket_, boost::asio::buffer(recv_buffer_), boost::asio::transfer_at_least(1), [self = shared_from_this()](const boost::system::error_code& error, size_t bytes_transferred) {
 		if (!error) {
 			self->send_buffer_ = string(self->recv_buffer_.data(), bytes_transferred);
-			self->room.broadcast(self->send_buffer_);
+			self->room.broadcast(self->send_buffer_, self->instance_number);
+			cout << "received: " << string(self->recv_buffer_.data(), bytes_transferred) << endl;
+			self->start();
 		}
 		else {
 			cerr << "ServerReadingError: " << error.message() << endl;
@@ -103,6 +99,7 @@ public:
 		auto new_session = chat_session::create(io_context_, room_);
 		acceptor_.async_accept(new_session->socket(), [this, new_session](const boost::system::error_code& error) {
 			if (!error) {
+				increment_instance++;
 				room_.join(new_session);
 				if (room_.get_sessions().size() == 1) {
 					boost::asio::async_write(new_session->socket(), boost::asio::buffer("<SERVER> Chat Room Started!"), [](const boost::system::error_code& error, size_t bytes_transfered) {
@@ -113,7 +110,6 @@ public:
 							cerr << "SERVER: " << error.message() << endl;
 						}
 						});
-					//new_session->self_send("SERVER: Chat Room Started!");
 				};
 				new_session->start();
 			}
